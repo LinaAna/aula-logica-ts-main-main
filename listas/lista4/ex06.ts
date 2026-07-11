606d276e1bfd138beb672b849961835939716af4
LinaAna <linacarolinadesouza1107@gmail.com>
2026-07-11 15:39:24 -0300

    Atualizar o ex06.ts

---
+++ listas/lista4/ex06.ts
@@ -0,0 +1,8 @@
+ex01-primeiro-ultimo.ts
+
+let nomes = ["João", "Maria", "Pedro", "Lúcia"];
+let primeiro = nomes[0];
+let ultimo = nomes[nomes.length - 1];
+
+alert("Primeiro: " + primeiro + "\nÚltimo: " + ultimo);
+
