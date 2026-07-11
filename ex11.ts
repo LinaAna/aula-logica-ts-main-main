680d3721ca79a6698a03c4619dd9c61e85762bce
LinaAna <linacarolinadesouza1107@gmail.com>
2026-07-11 15:47:36 -0300

    Excluir ex11.ts

--- ex11.ts
+++
@@ -1,8 +0,0 @@
-let nomes = ["carlos", "juliana"];
-let maiusculos = [];
-
-for (let i = 0; i < nomes.length; i++) {
-    maiusculos.push(nomes[i].toUpperCase());
-}
-
-alert(maiusculos.join(", "));
